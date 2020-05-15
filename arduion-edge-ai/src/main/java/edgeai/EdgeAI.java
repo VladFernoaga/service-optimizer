f72efafd9019d28aa0d8e1b3205d12a647b5d684
Vlad Fernoaga <fernoaga.vlad@gmail.com>
2020-05-15 14:41:19 +0300

    fix bugs

--- arduion-edge-ai/src/main/java/edgeai/EdgeAI.java
+++ arduion-edge-ai/src/main/java/edgeai/EdgeAI.java
@@ -1,14 +1,9 @@
 package edgeai;
-
 import java.util.Random;
-
 import org.sintef.jarduino.InvalidPinTypeException;
 import org.sintef.jarduino.JArduino;
-
 public class EdgeAI extends JArduino {
-
 	private WeightReader wReader = new LocalWeightReader();
-
 	// Read the weights for hidden layer1 already containing the additional bias
 	// parameters
 	double[][] layer1Weights = wReader.getWeightsHiddenLayer1();
@@ -17,48 +12,38 @@ public class EdgeAI extends JArduino {
 	double[][] layer2Weights = wReader.getWeightsHiddenLayer2();
 	// Read the weights for output already containing the additional bias parameters
 	double[][] outputLayerWeights = wReader.getWeightsOutputLayer();
-
 	Statistic[] statistics = wReader.getParameterStatistic();
-
 	@Override
 	protected void loop() throws InvalidPinTypeException {
 		// Run prediction using randomized inputs between range [0, 4095]
 		double[][] inputParams = getRandomizedInputParams();
-
-		// Print denormalized inputs
-		System.out.println("Denormalized");
+		// Print normalized inputs
+		System.out.println("Normalized");
 		System.out.println(inputParams[0][0]);
 		System.out.println(inputParams[1][0]);
 		System.out.println(inputParams[2][0]);
-
-		// Print normalized inputs
-		System.out.println("Normalized");
-		System.out.println((inputParams[0][0] - statistics[0].mean) / statistics[0].std);
-		System.out.println((inputParams[1][0] - statistics[1].mean) / statistics[1].std);
-		System.out.println((inputParams[2][0] - statistics[2].mean) / statistics[2].std);
-
-		double prediction = runPrediction(inputParams);
-
-		// Print normalized prediction
-		System.out.println("Normalized");
-		System.out.println((prediction - statistics[3].mean) / statistics[3].std);
-
-		// Print denormalized prediction
+		// Print denormalized inputs
 		System.out.println("Denormalized");
+		System.out.println(((inputParams[0][0] - 2048)/4096 * statistics[0].std) + statistics[0].mean);
+		System.out.println(((inputParams[1][0]  - 2048)/4096 * statistics[1].std) + statistics[1].mean);
+		System.out.println(((inputParams[2][0]  - 2048)/4096 * statistics[2].std) + statistics[2].mean);
+		double prediction = runPrediction(inputParams);
+		// Print normalized prediction
+		System.out.println("Normalized output");
 		System.out.println(prediction);
+		// Print denormalized prediction
+		System.out.println("Denormalized output");
+		System.out.println(prediction * statistics[3].std + statistics[3].mean);
 	}
 
 	double runPrediction(double[][] inputParams) {
 		// Run activations on first layer
 		double[][] outputLayer1 = multiply(inputParams, layer1Weights);
 		applyRelu(outputLayer1);
-
 		// Run Activations on second layer
 		double[][] outputLayer2 = multiply(outputLayer1, layer2Weights);
 		applyRelu(outputLayer2);
-
 		double predictionResult = multiply(outputLayer2, outputLayerWeights)[0][0];
-
 		return predictionResult;
 	}
 
@@ -92,8 +77,8 @@ public class EdgeAI extends JArduino {
 	private double[][] getRandomizedInputParams() {
 		double[][] inputParam = new double[4][1];
 		inputParam[0][0] = new Random().nextInt(4095); // PE
-		inputParam[1][0] = new Random().nextInt(4095); // ...
-		inputParam[2][0] = new Random().nextInt(4095); // ..
+		inputParam[1][0] = new Random().nextInt(4095); // RPM
+		inputParam[2][0] = new Random().nextInt(4095); // delta_p
 		inputParam[3][0] = 1; // Bias
 		return inputParam;
 	}
